438a8978de86aeee6fbbe76385da654eb4a2a40c
Dario Candia <darcanf.94@gmail.com>
2020-04-12 12:52:52 -0400

    100tifi.co

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,26 @@
 import Header from '../templates/header';
 import Home from '../pages/home';
-import Characters from '../pages/characters';
+import Character from '../pages/characters';
 import Error404 from '../pages/error404';
 import getHash from '../utils/getHash';
 import resolveRoutes from '../utils/resolveRoutes';
 
 const routes = {
     '/': Home,
-    '/:id':Characters,
+    '/:id':Character,
     '/Contact':'Contact',
 };
 
 const router = async () => {
     const header = null || document.getElementById('header');
-    const section = null || document.getElementById('section');
+    const content = null || document.getElementById('content');
 
     header.innerHTML = await Header();
 
     let hash = getHash();
     let route = await resolveRoutes(hash);
     let render = routes[route] ? routes[route] : Error404;
-    section.innerHTML = await render();
+    content.innerHTML = await render();
 };
 
 export default router;
